c34ef5cbff156b4d10515f37101955671d3cf110
Morten Sorensen <morten.sorensen.04@gmail.com>
2024-10-13 17:11:28 +0200

    added lab3

--- 2024H/embed/labs/lab3/input_test.c
+++ 2024H/embed/labs/lab3/input_test.c
@@ -5,22 +5,22 @@
 #include <stdio.h>
 
 int main() {
-    // Test of reading input from keyboard
-    int fd = open("/dev/input/event15", O_RDONLY);
-    if (fd == -1) {
-        perror("open");
-        return 1;
-    }
+	// Test of reading input from keyboard
+	int fd = open("/dev/input/event0", O_RDONLY);
+	if (fd == -1) {
+		perror("open");
+		return 1;
+	}
 
-    struct input_event ev;
-    while (1) {
-        if (read(fd, &ev, sizeof(struct input_event)) == -1) {
-            perror("read");
-            return 1;
-        }
-        if (ev.type == EV_KEY) {
-            printf("key %d state %d\n", ev.code, ev.value);
-        }
-    }
+	struct input_event ev;
+	while (1) {
+		if (read(fd, &ev, sizeof(struct input_event)) == -1) {
+			perror("read");
+			return 1;
+		}
+		if (ev.type == EV_KEY) {
+			printf("key %d state %d\n", ev.code, ev.value);
+		}
+	}
 
 }
