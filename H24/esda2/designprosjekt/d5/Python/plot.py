76de00a1c42b9f71fd525f93d3be4f1ac775f3d8
Morten Sørensen <morten.sorensen.04@gmail.com>
2025-01-11 19:45:43 +0100

    stuff

--- H24/esda2/designprosjekt/d5/Python/plot.py
+++ H24/esda2/designprosjekt/d5/Python/plot.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 
 # Load buffer_no_load.csv data
 # First column is time in seconds, second column is input voltage, third column is output voltage
-data = np.genfromtxt('../Data/buffer_distort_1V6.csv', delimiter=',', skip_header=1)
+name = "buffer_load_source"
+data = np.genfromtxt(f'../Data/{name}.csv', delimiter=',', skip_header=1)
 
 # Convert time to milliseconds
 data[:, 0] *= 1000
@@ -14,10 +15,14 @@ plot_end = len(data)
 plt.plot(data[middle:plot_end, 0], data[middle:plot_end, 2], label='$v_2(t)$')
 plt.plot(data[middle:plot_end, 0], data[middle:plot_end, 1], label='$v_0(t)$')
 
+print(np.max(data[:, 1]))
+print(np.max(data[:, 2]))
+
 # Add labels and title
+plt.title("$v_0(t)$ vs. $v_2(t)$ ved last $R_L = 220 \Omega$ og kilde motstand $R_K = 1.2$k$\Omega$")
 plt.xlabel('Tid (ms)')
 plt.ylabel('Spenning (V)')
 plt.legend()
 # plt.show()
-plt.savefig('../Notat/Bilder/buffer_distort_1V6.png')
+plt.savefig(f'../Notat/Bilder/{name}.png')
 
--- H24/esda2/designprosjekt/d7/Python/plot.py
+++ H24/esda2/designprosjekt/d7/Python/plot.py
@@ -6,7 +6,7 @@ from matplotlib.ticker import FixedLocator
 fsamp  = 9100
 fshalf = fsamp/2
 fsmin  = fshalf*0.75
-startf = 100  # frekvensen du begynnte målingen på
+startf = 50  # frekvensen du begynnte målingen på
 
 file = "../Data/working.csv"
 fullFilter = pd.read_csv(file, sep=',', skiprows=25, encoding='ISO-8859-1')
@@ -20,66 +20,60 @@ passY = [-3, -3, 3, 3]
 stopX = [fshalf, f[-1], f[-1], fshalf]
 stopY = [-100, -100, -10, -10]
 
-stageNames = ["working_stage_1", "working_stage_2"]
-stages = [pd.read_csv("../Data/" + file + ".csv", sep=',',  skiprows=25, encoding='ISO-8859-1') for file in stageNames]
-fs  = [np.array(i.iloc[:,0]) for i in stages]
-c1s = [np.array(i.iloc[:,1]) for i in stages]
-c2s = [np.array(i.iloc[:,2]) for i in stages]
+file2 = "../Data/working_longer.csv"
+fullFilter2 = pd.read_csv(file2, sep=',', skiprows=25, encoding='ISO-8859-1')
+f2  = np.array(fullFilter2.iloc[:,0])
+c1_2 = np.array(fullFilter2.iloc[:,1])
+c2_2 = np.array(fullFilter2.iloc[:,2])
 
+passX_2 = [startf, fsmin, fsmin, startf]
+passY_2 = [-3, -3, 3, 3]
+
+stopX_2 = [fshalf, f[-1], f[-1], fshalf]
+stopY_2 = [-100, -100, -10, -10]
+
+
+plt.figure(figsize=(12, 8))  # Øker størrelsen på hele plottet
+plt.subplots_adjust(hspace=0.5)  # Øker vertikal avstand mellom subplottene
 
 """  --- plot whole filter  ---  """
-plt.subplot(3,1,1)
+plt.subplot(2, 1, 1)
+plt.xscale("log")
+plt.fill(passX_2, passY_2, color='skyblue')  
+plt.fill(stopX_2, stopY_2, color='skyblue')  
+plt.plot([fsmin * 0.75, fsamp, fsamp, fsmin * 0.75, fsmin * 0.75], [3, 3, -20, -20, 3], color="red", linestyle="dashed", alpha=0.4)
+plt.plot(f2, c1_2, label="$v_1(f)$")
+plt.plot(f2, c2_2, label="$v_2(f)$")
+plt.title("Amplituderespons", fontsize=14)  # Større font på tittelen
+plt.ylim(-40, 5)  # Begrens y-aksen til 0 til -50
+plt.xlim(f2[0] * 0.9, f2[-1] * 1.1)
+plt.legend(loc="lower left")
+plt.xlabel("Frekvens (Hz)")
+plt.ylabel("Dempning (dBV)")
+
+"""  --- plot whole filter zoomed in  ---  """
+plt.subplot(2, 1, 2)
 plt.xscale("log")
 plt.fill(passX, passY, color='skyblue')  
 plt.fill(stopX, stopY, color='skyblue')  
-plt.plot([fsmin*0.75, fsamp*1.25, fsamp*1.25, fsmin*0.75, fsmin*0.75], [3, 3, -20, -20, 3], color = "red", linestyle = "dashed", alpha = 0.4)
-plt.plot(f, c1)
-plt.plot(f, c2)
-
-plt.title("Filter response")
-
-"""  ---  plot whole filter zoomed in  ---  """
-plt.subplot(3,1,2)
-plt.xscale("log")
-plt.fill(passX, passY, color='skyblue')  
-plt.fill(stopX, stopY, color='skyblue')  
-
-# dshed lines
-plt.plot([fsmin*0.75, fsamp*1.25], [-3,-3], color = "black", linestyle = "dashed", alpha = 0.4)
-plt.plot([fsmin*0.75, fsamp*1.25], [-10,-10], color = "black", linestyle = "dashed", alpha = 0.4)
-plt.plot([fsamp, fsamp], [-20,3], color = "black", linestyle = "dashed", alpha = 0.4)
-plt.plot([fshalf, fshalf], [-20,3], color = "black", linestyle = "dashed", alpha = 0.4)
-plt.plot([fsmin, fsmin], [-20,3], color = "black", linestyle = "dashed", alpha = 0.4)
-
-
-plt.plot(f, c1)
-plt.plot(f, c2)
-plt.xlim(fsmin*0.75, fsamp*1.25)
+plt.plot([fsmin * 0.75, fsamp * 1.25], [-3, -3], color="black", linestyle="dashed", alpha=0.4)
+plt.plot([fsmin * 0.75, fsamp * 1.25], [-10, -10], color="black", linestyle="dashed", alpha=0.4)
+plt.plot([fsamp, fsamp], [-20, 3], color="black", linestyle="dashed", alpha=0.4)
+plt.plot([fshalf, fshalf], [-20, 3], color="black", linestyle="dashed", alpha=0.4)
+plt.plot([fsmin, fsmin], [-20, 3], color="black", linestyle="dashed", alpha=0.4)
+plt.plot(f, c1, label="$v_1(f)$")
+plt.plot(f, c2, label="$v_2(f)$")
+plt.xlim(fsmin * 0.75, fsamp * 1.25)
 plt.ylim(-20, 3)
-plt.yticks([-3, -10],["-3", "-10"])
-plt.xticks([fsamp, fshalf, fsmin],["fs", "fs/2", "fs/2 * 0.75"])
+plt.yticks([-3, -10], ["-3", "-10"])
+plt.xticks([fsamp, fshalf, fsmin], ["$f_s$", "$f_s/2$", "$0.75 \cdot f_s/2$"])
 plt.gca().xaxis.set_major_locator(FixedLocator([fsamp, fshalf, fsmin]))
 plt.gca().xaxis.set_minor_locator(FixedLocator([]))
+plt.title("Amplituderespons forstørret", fontsize=14)  # Større font på tittelen
+plt.legend(loc="lower left")
+plt.xlabel("Frekvens (Hz)")
+plt.ylabel("Dempning (dBV)")
 
-plt.title("Filter response zoomed in")
-
-"""  ------  plot stages  -------  """
-plt.subplot(2,1,2)
-plt.xscale("log")
-plt.plot(fs[0], c1s[0])
-for i in range(0,2):
-    plt.plot(fs[i], c2s[i], label = stageNames[i])
-
-
-plt.title("The seperate stages")
-plt.legend()
-plt.rcParams["figure.figsize"]=(10,12)            
-plt.subplots_adjust(
-    top=0.88,
-    bottom=0.11,
-    left=0.125,
-    right=0.9,
-    hspace=0.295,
-    wspace=0.2
-)
-plt.show()
+plt.tight_layout()  # Fjern ekstra hvit plass rundt figuren
+# plt.show()
+plt.savefig("filter_freq_plot.png", dpi=300)
